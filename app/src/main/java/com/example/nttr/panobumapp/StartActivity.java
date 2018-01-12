e133e86a0a105370625a32ca61d323e2bb04c8e4
iskw <miteki.i53@gmail.com>
2018-01-12 16:37:27 +0900

    matisse library installed

--- app/src/main/java/com/example/nttr/panobumapp/StartActivity.java
+++ app/src/main/java/com/example/nttr/panobumapp/StartActivity.java
@@ -1,13 +1,29 @@
 package com.example.nttr.panobumapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button openButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        openButton = (Button) findViewById(R.id.open_folder_button);
+        openButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.open_folder_button:
+                Log.d("LOG", "button clicked");
+        }
+
     }
 }
