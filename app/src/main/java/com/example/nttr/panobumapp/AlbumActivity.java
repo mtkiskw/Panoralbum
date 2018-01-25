d8d67d86c99087b36ae33541b91f3d1e2b578d10
Miteki Ishikawa <miteki.i53@gmail.com>
2018-01-26 00:38:59 +0900

    uri null

--- app/src/main/java/com/example/nttr/panobumapp/AlbumActivity.java
+++ app/src/main/java/com/example/nttr/panobumapp/AlbumActivity.java
@@ -1,50 +1,72 @@
 package com.example.nttr.panobumapp;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class AlbumActivity extends AppCompatActivity {
     private ArrayList<Uri> selectedUris;
+    private ArrayList<Bitmap> bitmaps;
+    private ImageLoadingTask backgroundImageLoaderTask;
+    private VrPanoramaView panoWidgetView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
 
+//        Intent intent = new Intent(getApplication(), MainActivity.class);
+//        startActivity(intent);
+
         selectedUris = (ArrayList<Uri>)getIntent().getSerializableExtra("selectedUris");
         try {
-            showUris(selectedUris);
+            startPlaying(selectedUris);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        Button transActivityBtn = (Button) findViewById(R.id.show_album_btn);
-        transActivityBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), MainActivity.class);
-                startActivity(intent);
-            }
-
-        });
+//        Button transActivityBtn = (Button) findViewById(R.id.show_album_btn);
+//        transActivityBtn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplication(), MainActivity.class);
+//                startActivity(intent);
+//            }
+//
+//        });
     }
 
-    private void showUris(ArrayList<Uri> uris) throws IOException {
+    private void startPlaying(ArrayList<Uri> uris) throws IOException {
         for(int i=0; i<uris.size(); i++){
             Log.d("URI", uris.get(i).toString());
             Log.d("PATH", uris.get(i).getPath().toString());
+            try {
+                Bitmap b = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uris.get(i));
+                bitmaps.add(b);
+            }
+            catch (IOException e){
 
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uris.get(i));
+            }
         }
+
+//        ImageLoadingTask task = backgroundImageLoaderTask;
+//        if (task != null && !task.isCancelled()) {
+//            // Cancel any task from a previous loading.
+//            task.cancel(true);
+//        }
+//        VrPanoramaView.Options viewOptions = new VrPanoramaView.Options();
+//        viewOptions.inputType = VrPanoramaView.Options.TYPE_STEREO_OVER_UNDER;
+//        task = new ImageLoadingTask(panoWidgetView, viewOptions);
+//        task.execute(bitmaps.get(0));
+//        backgroundImageLoaderTask = task;
     }
 
 //    private File getFilePath(Uri uri){
---
+++ app/src/main/java/com/example/nttr/panobumapp/ImageLoadingTask.java
@@ -0,0 +1,40 @@
+package com.example.nttr.panobumapp;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
+import java.lang.ref.WeakReference;
+import java.net.URI;
+
+/**
+ * Created by ishikawa on 2018/01/25.
+ */
+
+public class ImageLoadingTask extends AsyncTask<Bitmap, Void, Bitmap>{
+    private static final String TAG = "ImageLoadingTask";
+    private final WeakReference<VrPanoramaView> viewReference; // 画像読み込み時にViewが壊れるのを防ぐ（回転させた時とか）
+    private final VrPanoramaView.Options viewOptions;
+
+    private static WeakReference<Bitmap> lastBitmap = new WeakReference<>(null);
+
+    protected Bitmap doInBackground(Bitmap... bitmaps) {
+        Bitmap b = bitmaps[0];
+        lastBitmap = new WeakReference<>(b);
+        return b;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        final VrPanoramaView vw = viewReference.get();
+        if (vw != null && bitmap != null) {
+            vw.loadImageFromBitmap(bitmap, viewOptions);
+        }
+    }
+
+    public ImageLoadingTask(VrPanoramaView view, VrPanoramaView.Options viewOptions) {
+        viewReference = new WeakReference<>(view);
+        this.viewOptions = viewOptions;
+    }
+}
